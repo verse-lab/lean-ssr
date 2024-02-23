d2000f50a9f56ac964bd2662bec7a6dfcc472523
volodeyka <vovaglad00@gmail.com>
2024-02-23 13:49:57 +0800

    fix goal annotation for ssrRevert

--- Ssreflect/Revert.lean
+++ Ssreflect/Revert.lean
@@ -32,7 +32,8 @@ def mkInitialTacticInfoR (stx : Syntax) : TacticM (TacticM Info) := do
 @[inline] def withTacticInfoContextR (stx : Syntax) (x : TacticM α) : TacticM α := do
   withInfoContext x (← mkInitialTacticInfoR stx)
 
-def elabSsrR : Tactic := fun stx =>
+def elabSsrR : Tactic := fun stx =>do
+  withTacticInfoContextR (<- getRef) do
   newTactic do
     match stx with
     | `(ssrRevert|$i:ident) => newTactic do
@@ -49,9 +50,10 @@ elab t:tactic ":" is:ssrReverts : tactic => do
   let is := mkNode `ssrReverts #[is]
   iterateElab (HashMap.ofList [
     (`ssrRevert, fun _ => elabSsrR),
-    (`ssrBasic, fun _ => elabBasic)
+    (`ssrBasic, fun _ stx => do
+      withTacticInfoContextR (<- getRef) $ elabBasic stx)
   ]) is; run `(tactic|$t);
 
 
--- example (k m : Int) : Int := by
---   skip: k (Eq.refl k)
+-- example (k m : Int) (eq : k = k) : k = k := by
+--   skip: {m} k (Eq.refl k)
