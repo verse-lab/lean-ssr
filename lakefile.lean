3f6085d9aa927883361c8edf14b7171ca7b10d39
volodeyka <vovaglad00@gmail.com>
2025-05-07 19:12:33 +0800

    fix version'

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ package ssreflect where
   -- add any additional package configuration options here
 
 require batteries from
-    git "https://github.com/leanprover-community/batteries" @ "v4.17.0"
+    git "https://github.com/leanprover-community/batteries" @ "v4.20.0-rc2"
 
 @[default_target]
 lean_lib Ssreflect {
